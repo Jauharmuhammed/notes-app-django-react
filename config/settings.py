0a2a4a370d173ddd211c97fb6b6fad1a2ac2e9c9
Jauhar Muhammed <jauhar@Jauhars-MacBook.local>
2022-11-25 15:45:47 +0530

    static files

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
     "corsheaders.middleware.CorsMiddleware",
 
