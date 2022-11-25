cc5aea81ccc1d38c20879ebcccb95a4149f28108
Jauhar Muhammed <jauhar@Jauhars-MacBook.local>
2022-11-25 10:15:26 +0530

    first commit

---
+++ api/views.py
@@ -0,0 +1,79 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+
+
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    return Response(routes)
+
+@api_view(['GET', 'POST'])
+def getNotes(request):
+    if request.method == 'GET':
+        notes = Note.objects.all().order_by('-updated')
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+
+    if request.method == 'POST':
+        data = request.data
+        note = Note.objects.create(
+            body=data['body']
+        )
+        serializer = NoteSerializer(note, many=False)
+        return Response(serializer.data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def getNote(request, pk):
+    if request.method == 'GET':
+        note = Note.objects.get(id=pk)
+        serializer = NoteSerializer(note, many=False)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        data = request.data
+        note = Note.objects.get(id=pk)
+        serializer = NoteSerializer(instance=note, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        note = Note.objects.get(id=pk)
+        note.delete()
+        return Response('Note Deleted Successfully')
+
