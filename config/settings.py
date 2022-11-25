a24da21b2bd6b29b61efc017e9789e918ee69539
Jauhar Muhammed <jauhar@Jauhars-MacBook.local>
2022-11-25 16:01:40 +0530

    static files

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2@bcc3sk4$j^i2x9*#b7%o0ms#ys9wc^mq!oc26$355qzxl^e@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
